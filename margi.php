a1d5c6ea0c80a6ada8481103845478d5b1aa0fdc
Matheus Vinicius <112036001+Mattttheus@users.noreply.github.com>
2023-08-11 21:06:53 -0300

    Add files via upload

---
+++ margi.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html>
+
+
+
+<head>
+    <meta charset="utf-8">
+    <title>Ambiente bootstrap</title>
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="stile.css">
+
+</head>
+
+<body>
+    <div class="contender">
+        <div class="row">
+            <div class="col-sm-3 logo">
+                <img src="baixados.jpeg" alt="..." class="rounded-circle">
+                <p class="text-center">
+                    <br>
+                    <b>M&J tech</b>
+                </p>
+            </div>
+
+            <div class="col-sm-7 menu">
+                <br>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand" href="#">Inicio</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Serviços<span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Produtos</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Pesquise
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">Acessorios</a>
+                                    <a class="dropdown-item" href="#">Peças</a>
+                                    <div class="dropdown-divider">contatos</div>
+                                    <a class="dropdown-item" href="#">Nossa Historia</a>
+                                </div>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#">Quem somos</a>
+                            </li>
+                        </ul>
+                        <form class="form-inline my-2 my-lg-0">
+                            <input class="form-control mr-sm-2" type="search" placeholder="Search"
+                                aria-label="Pesquise">
+                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">OK</button>
+                        </form>
+                    </div>
+                </nav>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 banner">
+                <img src="banner.png" class="" alt="Imagem responsiva">
+            </div>
+        </div>
+        <div class="row">
+
+            <div class="col-sm-3 margin-2 border-3 p1">
+                <img src="c++.jpeg" alt="..." class="rounded-circle">
+                <p><b>
+                        A linguagem de programação C++ utiliza bibliotecas de funções que auxiliam na criação e execução
+                        de funções que manipulam esses objetos. As bibliotecas de funções são conjuntos de procedimentos
+                        desenvolvidos para realizarem tarefas específicas diretamente no software.13 de out. de 2021
+
+                    </b>
+                </p>
+            </div>
+            <div class="col-sm-3 margin-2 border-3 p2 ">
+                <img src="fto.jpg" alt="..." class="rounded-circle">
+                <p>
+                    <b>O Photoshop é um software da multinacional americana Adobe Inc. usado para edição de
+                        imagens,
+                        criação de arte digital, design gráfico e animações. A multiplicidade de usos do
+                        software faz
+                        com que o Photoshop seja uma ferramenta de trabalho para diversos tipos de
+                        profissionais.
+                    </b>
+                </p>
+            </div>
+            <div class="col-sm-3 margin-2 border-3 p3 ">
+                <img src="pyth.png" alt="..." class="rounded-circle">
+                <p><b>O que aprender além de Python?
+                        Do Python você consegue ir pro Haskell ou implementar seu módulo com C/C++. Ruby/PHP
+                        e
+                        todos os
+                        sabores Web vc obrigatoriamente vai ter que saber um pouco de Sql, Javascript, Css e
+                        Html (pelo
+                        menos noções)
+                    </b>
+                </p>
+            </div>
+            <div class="col-sm-3 margin-2 border-3 p4 ">
+                <img src="html.jpeg" alt="..." class="rounded-circle">
+                <p><b>O que dá pra fazer com HTML5?
+                        O Que é HTML5 e Quais Vantagens ele Traz para seu Site
+                        Principais Vantagens do HTML5 para Desenvolvedores
+                        Manipulação de Erro Persistente. ...
+                        Semântica Melhorada para Elementos. ...
+                        Suporte Aprimorado para Recursos de Aplicativos da Web. ...
+                        O Elemento Canvas. ...
+                        O elemento Menu. ...
+                        Atributos de Dados Personalizáveis. ...
+                        Armazemento Local para Substituir Cookies.
+                    </b>
+                </p>
+            </div>
+        </div>
+        <div class="contender02">
+            <div class="row">
+                <div class="col-sm-8 midea01">
+                    <div class="audio">
+                        <audio controls>
+                            <source src="https://www.mediacollege.com/downloads/sound-effects/alien/laser-01.mp3">
+                        </audio>
+                        <br>
+                        <audio controls>
+                            <source src="https://www.mediacollege.com/downloads/sound-effects/alien/sweep2.mp3">
+                        </audio>
+                        <br>
+                        <audio controls>
+                            <source src="https://www.mediacollege.com/downloads/sound-effects/alien/sweep2.mp3">
+                        </audio>
+                        <br>
+                        <audio controls>
+                            <source src="https://www.mediacollege.com/downloads/sound-effects/alien/sweep2.mp3">
+                        </audio>
+                        <br>
+                        <audio controls>
+                            <source src="https://www.mediacollege.com/downloads/sound-effects/alien/sweep2.mp3">
+                        </audio>
+                    </div>
+                </div>
+                <div class="col-sm-4 margin-1 border-3 midea2">
+                    <iframe width="400" height="350" src="https://www.youtube.com/embed/ko5lL2MQKCU"
+                        title="YouTube video player" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <script src=" js/bootstrap.min.js">
+    </script>
+</body>
+
+</html>
